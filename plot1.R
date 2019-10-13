e2b79aaf916c3a03a291b4ab99152969fb16560b
SAKSHI JAIN <52284249+sakshij25@users.noreply.github.com>
2019-10-13 12:54:04 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,8 @@
+data_full <- read.csv("C:/Users/Prachi/Downloads/exdata_data_household_power_consumption/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+hist(data1$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
